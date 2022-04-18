f3f63576e4a5348f5619eeda50b80485caa52a27
nullptr <3621629+0x0L@users.noreply.github.com>
2022-04-19 00:43:50 +0200

    removed dateutil

--- showrss_helper.py
+++ showrss_helper.py
@@ -3,55 +3,58 @@
 An OSX tool to automate downloads from showRSS
 """
 
-from dateutil import parser
+from datetime import datetime
 from os import path
 from subprocess import call
 from xml.dom import minidom
-
-try:
-    from urllib2 import build_opener
-except ImportError:
-    from urllib.request import build_opener
+from urllib.request import build_opener
 
 
-RSS_FEED_URL = """FEED_URL_CONFIG"""
+URL = """http://showrss.info/user/35118.rss?magnets=true&namespaces=true&name=clean&quality=null&re=no"""
 
-STAMP_FILE = path.join(path.expanduser("~"), '.showrss')
+STAMP_FILE = path.join(path.expanduser("~"), ".showrss")
+
+
+def parse_date(date):
+    return
 
 
 def read_url(url):
     opener = build_opener()
-    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    opener.addheaders = [("User-agent", "Mozilla/5.0")]
     return opener.open(url, timeout=10).read()
 
 
 def parse_feed(feed):
     get = minidom.parseString(feed).getElementsByTagName
-    dates = (parser.parse(z.firstChild.nodeValue) for z in get('pubDate'))
-    links = (z.firstChild.nodeValue for z in get('link')[1:])
+    dates = (
+        datetime.strptime(z.firstChild.nodeValue, "%a, %d %b %Y %H:%M:%S %z")
+        for z in get("pubDate")
+    )
+    links = (z.firstChild.nodeValue for z in get("link")[1:])
     return zip(dates, links)
 
 
 def get_timestamp():
-    dt = '2000-01-01 00:00:00+00:00'
+    dt = "2000-01-01 00:00:00+00:00"
     if path.exists(STAMP_FILE):
-        with open(STAMP_FILE, 'r') as stamp_file:
+        with open(STAMP_FILE, "r") as stamp_file:
             dt = stamp_file.read()
-    return parser.parse(dt)
+    return datetime.strptime(dt, "%Y-%m-%d %H:%M:%S%z")
 
 
 def write_timestamp(dt):
-    with open(STAMP_FILE, 'w') as stamp_file:
+    with open(STAMP_FILE, "w") as stamp_file:
         stamp_file.write(str(dt))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     timestamp = get_timestamp()
-    all_shows = parse_feed(read_url(RSS_FEED_URL))
+    all_shows = parse_feed(read_url(URL))
     new_shows = [x for x in all_shows if x[0] > timestamp]
 
     for _, link in new_shows:
-        call(['open', '-g', link])
+        call(["open", "-g", link])
 
     if new_shows:
         times, _ = zip(*new_shows)
